a34543ded058e3eaa995e5f0a0a329eee5b26aa5
Basel Al-Jabari <baslimadj@gmail.com>
2024-11-21 21:12:27 +0200

    Create Euler's Tour 3 - Segment Tree on a Tree.cpp

---
+++ Graph and Tree/Euler's Tour 3 - Segment Tree on a Tree.cpp
@@ -0,0 +1,184 @@
+// Given a tree with n nodes, q queries, there's two types of queries:
+// 1. Update the value of node s to value x.
+// 2. Output the sum of the subtree of node s.
+
+// قال رسول الله صلى الله عليه وسلم: "مَن تواضَعَ للهِ رَفَعَهُ".
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <chrono>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace chrono;
+
+#define endl '\n'
+#define ll long long
+
+template<typename data_type>
+using ordered_set = tree<data_type, null_type, less<data_type>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937 rng(high_resolution_clock::now().time_since_epoch().count());
+
+int dx[8] = { 0, 1, 0, -1, -1, 1, 1, -1 };
+int dy[8] = { 1, 0, -1, 0, 1, 1, -1, -1 };
+
+vector<vector<int>> g;
+vector<int> in, out, a;
+int timer;
+
+struct segment
+{
+	int n;
+	vector<ll> st;
+
+	segment(int n)
+	{
+		this->n = n;
+		st.resize(n * 4 + 1, 0);
+	}
+
+	void build(int s, int e, int node, const vector<int> &arr)
+	{
+		if (s == e)
+		{
+			st[node] = arr[s];
+			return;
+		}
+
+		int mid = (s + e) >> 1;
+		build(s, mid, 2 * node, arr);
+		build(mid + 1, e, 2 * node + 1, arr);
+
+		st[node] = st[2 * node] + st[2 * node + 1];
+	}
+
+	void build(const vector<int> &arr)
+	{
+		build(1, n, 1, arr);
+	}
+
+	ll query(int s, int e, int node, int rs, int re)
+	{
+		if (s > re || e < rs)
+			return 0;
+
+		if (s >= rs and e <= re)
+			return st[node];
+
+		int mid = (s + e) >> 1;
+		ll q1 = query(s, mid, 2 * node, rs, re);
+		ll q2 = query(mid + 1, e, 2 * node + 1, rs, re);
+		return q1 + q2;
+	}
+
+	ll query(int rs, int re)
+	{
+		return query(1, n, 1, rs, re);
+	}
+
+	void update(int s, int e, int node, int leaf, int val)
+	{
+		if (s == leaf and e == leaf)
+		{
+			st[node] = val;
+			return;
+		}
+
+		int mid = (s + e) >> 1;
+		if (leaf <= mid)
+			update(s, mid, 2 * node, leaf, val);
+		else
+			update(mid + 1, e, 2 * node + 1, leaf, val);
+
+		st[node] = st[2 * node] + st[2 * node + 1];
+	}
+
+	void update(int leaf, int val)
+	{
+		update(1, n, 1, leaf, val);
+	}
+};
+
+void dfs(int node, int par)
+{
+	in[node] = ++timer;
+
+	for (auto c : g[node])
+		if (c != par)
+			dfs(c, node);
+
+	out[node] = timer;
+}
+
+void Solve()
+{
+	int n, q;
+	cin >> n >> q;
+
+	// 1-base index.
+	g.assign(n + 1, {});
+	in.assign(n + 1, 0);
+	out.assign(n + 1, 0);
+	a.assign(n + 1, 0);
+	timer = 0;
+
+	for (int i = 1; i <= n; ++i)
+		cin >> a[i];
+
+	for (int i = 0; i < n - 1; ++i)
+	{
+		int x, y;
+		cin >> x >> y;
+
+		g[x].push_back(y);
+		g[y].push_back(x);
+	}
+
+	dfs(1, 0);
+	vector<int> ordered_array(n + 1); // This is the values of a sorted depending on in time.
+	for (int i = 1; i <= n; ++i)
+		ordered_array[in[i]] = a[i];
+
+	segment tree(n);
+	tree.build(ordered_array);
+
+	while (q--)
+	{
+		int type;
+		cin >> type;
+
+		if (type == 1)
+		{
+			int s, x;
+			cin >> s >> x;
+			tree.update(in[s], x);
+		}
+
+		else
+		{
+			int s;
+			cin >> s;
+			cout << tree.query(in[s], out[s]) << endl;
+		}
+	}
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("C:\\Sublime\\input.txt", "r", stdin);
+	freopen("C:\\Sublime\\output.txt", "w", stdout);
+#endif
+
+	Solve();
+
+	cerr << "Basel Al-Jabari." << endl;
+
+	return 0;
+}
