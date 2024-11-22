5bd0eb59bf7a710123d28351b85e1e5a1d07de86
Basel Al-Jabari <baslimadj@gmail.com>
2024-11-22 18:26:02 +0200

    Update Strongly Connected Components (Kosaraju's Algorithm).cpp

--- Graph and Tree/Strongly Connected Components (Kosaraju's Algorithm).cpp
+++ Graph and Tree/Strongly Connected Components (Kosaraju's Algorithm).cpp
@@ -1,4 +1,4 @@
-// قال رسول الله صلى الله عليه وسلم: "مَن تواضَعَ للهِ رَفَعَهُ".
+// The Messenger of Allah (Peace and blessings be upon him) said: "Whoever is humble for the sake of Allah, Allah will raise him".
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
