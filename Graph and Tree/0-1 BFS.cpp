af909ff640cfe79f97c46dee532c78a547457568
Basel Al-Jabari <baslimadj@gmail.com>
2024-11-23 20:22:14 +0200

    Create 0-1 BFS.cpp

---
+++ Graph and Tree/0-1 BFS.cpp
@@ -0,0 +1,89 @@
+// Given a weighted undirected graph with n nodes, and m edges (1-base index).
+// Output the shortest path from the node 1 and other nodes.
+// The weights are either 0 or 1.
+
+// The Messenger of Allah (Peace and blessings be upon him) said: "Whoever is humble for the sake of Allah, Allah will raise him".
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <chrono>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace chrono;
+
+#define endl '\n'
+#define ll long long
+
+template<typename data_type>
+using ordered_set = tree<data_type, null_type, less<data_type>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937 rng(high_resolution_clock::now().time_since_epoch().count());
+
+int dx[8] = { 0, 1, 0, -1, -1, 1, 1, -1 };
+int dy[8] = { 1, 0, -1, 0, 1, 1, -1, -1 };
+
+vector<vector<pair<int, int>>> g;
+vector<int> dist;
+
+void Solve()
+{
+	int n, m;
+	cin >> n >> m;
+
+	g.assign(n + 1, {});
+	dist.assign(n + 1, INT_MAX);
+
+	for (int i = 0; i < m; ++i)
+	{
+		int x, y, w;
+		cin >> x >> y >> w;
+
+		g[x].push_back(make_pair(y, w));
+		g[y].push_back(make_pair(x, w));
+	}
+
+	deque<pair<int, int>> dijk;
+	dijk.push_back(make_pair(0, 1)); // Weight 0, Node 1.
+	dist[1] = INT_MIN;
+	while (dijk.size())
+	{
+		int current = dijk.front().first, node = dijk.front().second;
+		dijk.pop_front();
+
+		for (auto [c, w] : g[node])
+			if (current + w < dist[c])
+			{
+				dist[c] = current + w;
+
+				if (w)
+					dijk.push_back(make_pair(dist[c], c));
+				else
+					dijk.push_front(make_pair(dist[c], c));
+			}
+	}
+
+	// The shortest path between node 1 and the other nodes.
+	for (int i = 2; i <= n; ++i)
+		cout << dist[i] << ' ';
+	cout << endl;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("C:\\Sublime\\input.txt", "r", stdin);
+	freopen("C:\\Sublime\\output.txt", "w", stdout);
+#endif
+
+	Solve();
+
+	cerr << "Basel Al-Jabari." << endl;
+
+	return 0;
+}
