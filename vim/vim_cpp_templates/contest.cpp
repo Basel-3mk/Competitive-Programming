0f604f5036d6232f4cf418b8d11347d98896f7f5
Basel Al-Jabari <baslimadj@gmail.com>
2025-07-29 10:32:04 +0300

    Add files via upload

---
+++ vim/vim_cpp_templates/contest.cpp
@@ -0,0 +1,31 @@
+// The Messenger of Allah (Peace and blessings be upon him) said: "Whoever is humble for the sake of Allah, Allah will raise him".
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define ll long long
+
+template<typename data_type>
+using ordered_set = tree<data_type, null_type, less<data_type>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int dx[16] = { +0, +0, -1, +1, -1, -1, +1, +1, -1, -2, -1, -2, +1, +2, +1, +2 }; 
+int dy[16] = { +1, -1, +0, +0, +1, -1, +1, -1, +2, +1, -2, -1, +2, +1, -2, -1 };
+
+void Solve() {
+
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	Solve();
+
+	return 0;
+}
