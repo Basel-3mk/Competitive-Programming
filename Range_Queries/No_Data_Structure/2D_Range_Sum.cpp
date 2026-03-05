9d771fd798313b4e7db7a903651c46879c1e2425
Basel Al-Jabari <baslimadj@gmail.com>
2026-03-05 22:48:05 +0200

    Rewriting 2D Prefix Sum & 2D Range Sum

--- Range_Queries/No_Data_Structure/2D_Range_Sum.cpp
+++ Range_Queries/No_Data_Structure/2D_Range_Sum.cpp
@@ -1,80 +1,76 @@
-// https://vjudge.net/problem/USACO-919
-
 // The Messenger of Allah (Peace and blessings be upon him) said: "Whoever is humble for the sake of Allah, Allah will raise him".
 
 #include <bits/stdc++.h>
-#include <bits/extc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
 using namespace __gnu_pbds;
-using namespace chrono;
 
 #define endl '\n'
 #define ll long long
 
-template<typename data_type>
+template <typename data_type>
 using ordered_set = tree<data_type, null_type, less<data_type>, rb_tree_tag, tree_order_statistics_node_update>;
 
-mt19937 rng(high_resolution_clock::now().time_since_epoch().count());
+int dx[16] = {0, 1, 0, -1, -1, 1, 1, -1, -1, -1, +1, +1, -2, -2, +2, +2};
+int dy[16] = {1, 0, -1, 0, 1, 1, -1, -1, -2, +2, -2, +2, -1, +1, -1, +1};
 
-int dx[8] = { 0, 1, 0, -1, -1, 1, 1, -1 };
-int dy[8] = { 1, 0, -1, 0, 1, 1, -1, -1 };
+void Solve() {
+  int n, m, q;
+  cin >> n >> m >> q;
 
-const int N = 1003;
+  vector<vector<int>> a(n + 1, vector<int>(m + 1));
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      cin >> a[i][j];
+    }
+  }
 
-int arr[N][N];
+  vector<vector<ll>> preSum(n + 2, vector<ll>(m + 2));
+  for (int i = 1; i <= q; ++i) {
+    array<int, 2> row, column;
+    for (int j = 0; j < 2; ++j) {
+      cin >> row[j] >> column[j];
+    }
 
-void Solve()
-{
-	memset(arr, 0, sizeof(arr));
+    int val;
+    cin >> val;
 
-	int n, k;
-	cin >> n >> k;
+    preSum[row[0]][column[0]] += val;
+    preSum[row[1] + 1][column[1] + 1] += val;
 
-	for (int i = 0; i < n; ++i)
-	{
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
+    preSum[row[0]][column[1] + 1] -= val;
+    preSum[row[1] + 1][column[0]] -= val;
+  }
 
-		x1++;
-		y1++;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      preSum[i][j] += preSum[i - 1][j] + preSum[i][j - 1] - preSum[i - 1][j - 1];
+    }
+  }
 
-		arr[y1][x1]++;
-		arr[y2 + 1][x2 + 1]++;
-		arr[y1][x2 + 1]--;
-		arr[y2 + 1][x1]--;
-	}
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      cout << a[i][j] + preSum[i][j] << ' ';
+    }
 
-	for (int i = 1; i < N; ++i)
-		for (int j = 1; j < N; ++j)
-			arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
-
-	int ans = 0;
-	for (int i = 1; i < N; ++i)
-		for (int j = 1; j < N; ++j)
-			if (arr[i][j] == k)
-				ans++;
-
-	cout << ans << endl;
+    cout << endl;
+  }
 }
 
-int32_t main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	freopen("paintbarn.in", "r", stdin);
-	freopen("paintbarn.out", "w", stdout);
+  int t = 1;
+  // cin >> t;
+  for (int i = 1; i <= t; ++i) {
+    Solve();
+  }
 
-	int t = 1;
-	// cin >> t;
-	for (int i = 0; i < t; ++i)
-	{
-		Solve();
-	}
+  cerr << "Basel Al-Jabari." << endl;
 
-	cerr << "Basel Al-Jabari." << endl;
-
-	return 0;
+  return 0;
 }
